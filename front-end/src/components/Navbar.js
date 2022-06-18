9542255d3eda1bfd96a9a3c5bc3c41ce2f92629f
fleaBag13 <dyutika.khatate@gmail.com>
2022-06-18 14:15:51 +0530

    included stylesheet

--- front-end/src/components/Navbar.js
+++ front-end/src/components/Navbar.js
@@ -1,13 +1,25 @@
-import {Link} from 'react-router-dom'
+//import {Link} from 'react-router-dom'
 
 const Navbar = () => {
 
     return (
         <header>
             <div className='container'>
-                <Link to="/">
+                {/* <Link to="/">
                     <h1>ArtWebsite</h1>
-                </Link>
+                </Link> */}
+                <h1>ArtWebsite</h1>
+            </div>
+            <div>
+            <div className="Menubar">
+            <ul>
+                <li className="active"><i className="fa fa-home" ></i> Home</li>
+                <li><i className="fa fa-id-card"></i> Resume</li>
+                <li><i className="fa fa-window-maximize"></i> Forums</li>
+                <li><i className="fa fa-user-plus"></i>Login</li>
+                <li><i className="fa fa-info" ></i>Subscribe</li>
+            </ul>
+            </div>
             </div>
         </header>
     )
