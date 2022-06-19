6f02dd8570af4e730426e3f76c9c233a791b79a8
Rajkamal <rajkamal.singh1011@gmail.com>
2022-06-19 19:29:21 +0530

    done with sculpture and pottery

--- front-end/src/components/Home/Home.js
+++ front-end/src/components/Home/Home.js
@@ -1,45 +1,59 @@
-import React from 'react';
+import React from "react";
 
-import Posts from '../Posts/Posts';
+import Posts from "../Posts/Posts";
 
 const Home = () => {
+  const posts = [
+    {
+      title: "ABC",
+      price: "100",
+      artist: "Dyutika",
+      desc: "lorem ipsum",
+      imgUrl: "http://bit.ly/2tMBBTd",
+    },
+    {
+      title: "XYZ",
+      price: "70",
+      artist: "Aryan",
+      desc: "lorem ipsum",
+      imgUrl: "http://bit.ly/2tMBBTd",
+    },
+    {
+      title: "PQR",
+      price: "30",
+      artist: "Vaibhavi",
+      desc: "lorem ipsum",
+      imgUrl: "http://bit.ly/2tMBBTd",
+    },
+    {
+      title: "MNO",
+      price: "50",
+      artist: "Rajkamal",
+      desc: "lorem ipsum",
+    },
+    {
+      title: "MNO",
+      price: "50",
+      artist: "Rajkamal",
+      desc: "lorem ipsum",
+    },
+  ];
 
-    const posts = [
-        {
-            title: "ABC",
-            price: "100",
-            artist: "Dyutika",
-            desc: "lorem ipsum"
-        },
-        {
-            title: "XYZ",
-            price: '70',
-            artist: "Aryan",
-            desc: "lorem ipsum"
-        },
-        {
-            title: "PQR",
-            price: "30",
-            artist: "Vaibhavi",
-            desc: "lorem ipsum"
-        },
-        {
-            title: "MNO",
-            price: "50",
-            artist: "Rajkamal",
-            desc: "lorem ipsum"
-        }
-    ]
-
-    return(
-        <>
-            <div className='grid'>
-                {posts.map((post) => (
-                    <Posts title={post.title} artist={post.artist} price={post.price} desc={post.desc}/>
-                ))}
-            </div>
-        </>
-    );
-}
+  return (
+    <>
+      <div className="grid">
+        {posts.map((post) => (
+          <Posts
+            title={post.title}
+            artist={post.artist}
+            price={post.price}
+            desc={post.desc}
+            imgUrl={post.imgUrl}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
 
 export default Home;
